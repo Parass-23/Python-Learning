aec12ce054bf662c077012b6cc1eaa5e71c7b4c3
Paras Nagdevee <parasnag2008@gmail.com>
2026-09-16 18:47:16 +0530

    updategst

--- numpy/basic_numpy.py
+++ numpy/basic_numpy.py
@@ -4,3 +4,4 @@ bonus=10
 marks=np.array([1,2,3,4,5])
 new_marks=marks+bonus
 print(new_marks)
+
---
+++ numpy/updategst.py
@@ -0,0 +1,5 @@
+import numpy as np
+prices=[50,100,150,40,80]
+p_np=np.array(prices)
+gst_np=p_np+0.18*p_np
+print(gst_np)
